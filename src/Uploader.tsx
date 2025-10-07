695e94767f1397f84a3fcbb51d87df65ae5b1483
ASalek-Szkola <antoni.salek@tm1.edu.pl>
2025-10-07 09:27:14 +0200

    School work

--- src/Uploader.tsx
+++ src/Uploader.tsx
@@ -5,20 +5,18 @@ import { useState, useRef, useEffect } from 'react';
 
 interface PathStyle {
     fill?: string;
-    stroke?: string;
-    strokeWidth?: string;
 }
 
 export function Uploader() {
     const containerRef = useRef<HTMLDivElement>(null);
     const inputRefs = useRef<Array<HTMLInputElement | null>>([]);
-    const [svgContent, setSvgContent] = useState<string | null>(null);
-    const [paths, setPaths] = useState<SVGPathElement[]>([]);
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
-    // Store a copy of the original SVG string and path styles
+    const [svgContent, setSvgContent] = useState<string | null>(null);
     const [originalSvgContent, setOriginalSvgContent] = useState<string | null>(null);
     const [initialPathStyle, setInitialPathStyle] = useState<PathStyle[]>([]);
+    const [paths, setPaths] = useState<SVGPathElement[]>([]);
+
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
     const handleDrop = (acceptedFiles: File[]) => {
         const file = acceptedFiles[0];
@@ -28,6 +26,7 @@ export function Uploader() {
                 const text = e.target?.result as string;
                 setSvgContent(text);
                 setOriginalSvgContent(text); // Save original SVG for later use
+
                 // Parse SVG and extract path elements
                 try {
                     const parser = new DOMParser();
@@ -37,8 +36,6 @@ export function Uploader() {
 
                     setInitialPathStyle(Array.from(pathNodes).map(path => ({
                         fill: path.style.fill || '',
-                        stroke: path.style.stroke || '',
-                        strokeWidth: path.style.strokeWidth || ''
                     })));
                 } catch (err) {
                     setPaths([]);
@@ -62,24 +59,16 @@ export function Uploader() {
             path.style.transition = 'fill 0.2s, stroke 0.2s';
             if (hoveredIndex === idx) {
                 path.style.fill = '#ffe5b4';
-                // path.style.stroke = 'orange';
-                path.style.strokeWidth = '3';
             } else {
                 path.style.fill = initialPathStyle[idx]?.fill || '';
-                // path.style.stroke = initialPathStyle[idx]?.stroke || '';
-                path.style.strokeWidth = initialPathStyle[idx]?.strokeWidth || '';
             }
             path.onclick = null;
             path.onclick = () => {
                 inputRefs.current[idx]?.focus();
                 svgPaths.forEach((p, j) => {
                     p.style.fill = initialPathStyle[j]?.fill || '';
-                    // p.style.stroke = initialPathStyle[j]?.stroke || '';
-                    p.style.strokeWidth = initialPathStyle[j]?.strokeWidth || '';
                 });
                 path.style.fill = '#ffe5b4';
-                // path.style.stroke = 'orange';
-                path.style.strokeWidth = '3';
             };
         });
     }, [hoveredIndex, svgContent, paths, initialPathStyle]);
@@ -87,7 +76,7 @@ export function Uploader() {
     return (
         <div className="uploader-container">
             {!svgContent && paths.length === 0 &&
-                <Dropzone accept={{'image/svg+xml': ['.svg']}} onDrop={handleDrop}>
+                <Dropzone accept={{ 'image/svg+xml': ['.svg'] }} onDrop={handleDrop}>
                     {({ getRootProps, getInputProps }) => (
                         <section>
                             <div {...getRootProps()} className="uploader-dropzone">
@@ -99,51 +88,61 @@ export function Uploader() {
                 </Dropzone>
             }
             {svgContent && paths.length > 0 && (
-                <div className='uploader-content'>
+                <>
                     <div className="uploader-svg-preview">
                         <div
                             ref={containerRef}
-                            dangerouslySetInnerHTML={{ __html: svgContent }}
+                            dangerouslySetInnerHTML={{ __html: svgContent }} // Load SVG into HTML
                         />
                     </div>
 
-                    <div className='uploader-input-list'>
-                        {paths.map((path: SVGPathElement, i: number) => (
-                            <input
-                                type="text"
-                                key={i}
-                                placeholder={path.id || ''}
-                                ref={el => { inputRefs.current[i] = el; }}
-                                onFocus={() => setHoveredIndex(i)}
-                            />
-                        ))}
-                    </div>
+                    <div className='uploader-interaction'>
+                        <div className='uploader-input-list'>
+                            {paths.map((path: SVGPathElement, i: number) => (
+                                <input
+                                    type="text"
+                                    key={i}
+                                    placeholder={path.id || ''}
+                                    ref={el => { inputRefs.current[i] = el; }}
+                                    onFocus={() => setHoveredIndex(i)}
+                                />
+                            ))}
+                        </div>
 
-                    <div className='uploader-btn'>
-                        <button onClick={() => {
-                            if (!originalSvgContent) return;
-                            // Parse original SVG
-                            const parser = new DOMParser();
-                            const doc = parser.parseFromString(originalSvgContent, 'image/svg+xml');
-                            const pathNodes = doc.querySelectorAll('path');
-                            // Replace ids with input values
-                            pathNodes.forEach((path, i) => {
-                                path.setAttribute('id', inputRefs.current[i]?.value || path.id || '');
-                            });
-                            // Serialize SVG
-                            const serializer = new XMLSerializer();
-                            const newSvg = serializer.serializeToString(doc.documentElement);
-                            const svgBlob = new Blob([newSvg], { type: 'image/svg+xml' });
-                            const svgUrl = URL.createObjectURL(svgBlob);
+                        <div className='uploader-btn-div'>
+                            <button className='uploader-btn' onClick={() => {
+                                if (!originalSvgContent) return;
 
-                            const newTab = window.open();
-                            if (newTab) {
-                                newTab.document.write(`<iframe src="${svgUrl}" frameborder="0" style="width:100%;height:100%;" />`);
-                                newTab.document.close();
-                            }
-                        }}>Wyślij</button>
+                                // Parse original SVG
+                                const parser = new DOMParser();
+                                const doc = parser.parseFromString(originalSvgContent, 'image/svg+xml');
+                                const pathNodes = doc.querySelectorAll('path');
+
+                                // Replace ids with input values
+                                pathNodes.forEach((path, i) => {
+                                    path.setAttribute('id', inputRefs.current[i]?.value || path.id || '');
+                                });
+
+                                // Serialize SVG
+                                const serializer = new XMLSerializer();
+                                const newSvg = serializer.serializeToString(doc.documentElement);
+                                const svgBlob = new Blob([newSvg], { type: 'image/svg+xml' });
+                                const svgUrl = URL.createObjectURL(svgBlob);
+
+                                const newTab = window.open();
+                                if (newTab) {
+                                    newTab.document.write(`<iframe src="${svgUrl}" frameborder="0" style="width:100%;height:100%;" />`);
+                                    newTab.document.close();
+                                }
+
+                                /* TODO:
+                                 - Write empty id if input corresponding to path is empty
+                                 - Add ignore button for each input (write empty id for corresponding path)
+                                */
+                            }}>Wyślij</button>
+                        </div>
                     </div>
-                </div>    
+                </>
             )}
         </div>
     );
